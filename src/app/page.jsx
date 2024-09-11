ff83ebc4921194a8bb7370cd487a15df572cb99a
altan-ab <altan.abduloglu@gmail.com>
2024-09-11 21:54:15 +0300

    Initial-setup

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+'use client'
+import React, { StrictMode } from 'react'
+
+import App from '@/App'
+
+const Home = () => {
+  return (
+    <div>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </div>
+  )
+}
+export default Home
